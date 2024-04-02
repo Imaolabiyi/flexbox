5651d42a09b8c012c55dd561ed7e6ea340ac9ede
imaolabiyi <sheron4krist@yahoo.com>
2024-04-02 18:41:42 +0100

    initial commit

---
+++ src/Flexbox.jsx
@@ -0,0 +1,33 @@
+import './Flexbox.css'
+
+function Flexbox() {
+    return (
+        <div className="container-flex">
+            <div className='flex_container'>
+                <h1 className='tab1'>Phillip</h1>
+                <h1 className='tab2'>Nathaniel</h1>
+                <h1>Martin</h1>
+            </div>
+            <div className='grid_container'>
+                <h1 className='grid grid1'>Abraham</h1>
+                <h1 className='grid grid2'>Shedrack</h1>
+                <h1 className='grid grid3'>Sharon</h1>
+                <h1 className='grid grid4'>Brayden</h1>
+                <h1 className='grid grid5'>Tariq</h1>
+                <h1 className='grid grid6'>Lauren</h1>
+                <h1 className='grid grid7'>Vanessa</h1>
+                <h1 className='grid grid8'>Jeremiah</h1>
+                <h1 className='grid grid5'>Tariq</h1>
+                <h1 className='grid grid6'>Lauren</h1>
+                <h1 className='grid grid7'>Vanessa</h1>
+                <h1 className='grid grid8'>Jeremiah</h1>
+                <h1 className='grid grid1'>Abraham</h1>
+                <h1 className='grid grid2'>Shedrack</h1>
+                <h1 className='grid grid3'>Sharon</h1>
+                <h1 className='grid grid4'>Brayden</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Flexbox
